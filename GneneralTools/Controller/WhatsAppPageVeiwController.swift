2eb86e91208d0064bf086ca36841aa1222bec7bc
SalmanMohammedDev <s.naffa77@gmail.com>
2020-11-22 23:36:42 +0300

    Add the the shortcut btnfor whatsapp

---
+++ GneneralTools/Controller/WhatsAppPageVeiwController.swift
@@ -0,0 +1,9 @@
+//
+//  WhatsAppPageVeiwController.swift
+//  GeneralTools
+//
+//  Created by Salman Mohammed on 07/04/1442 AH.
+//  Copyright © 1442 Salman Mohammed. All rights reserved.
+//
+
+import Foundation
